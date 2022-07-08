726b7f5203d8d4afb76c2b083c554dc842fc8a13
trainingsrepo <trainings.anil@gmail.com>
2022-07-08 06:30:59 +0530

    feature 302 changes in app.java file by developer2

--- app.java
+++ app.java
@@ -22,4 +22,10 @@ class Main {
       e.getStackTrace();
     }
   }
+  #feature302 changes by developer2
+  public static void sampleMethod(int a, int b) throws ArithmeticException{
+        System.out.println("Hello, this is sample method");
+        int c = a/b; //  int c = 4/2 which gets calculated as 2 
+        System.out.println("c:"+c); // c: 2
+   }
 }
